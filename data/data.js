062cc8e87dc77106c65b2449e0e3af9bc144fadf
ayushghetia <55269750+ghetiaayush@users.noreply.github.com>
2024-07-14 11:43:30 +0530

    initial commit

--- data/data.js
+++ data/data.js
@@ -1,6 +1,8 @@
 const BOOKS_DATA = [
   {id: "1", title: 'The Final Empire', author: 'Brandon Sanderson'},
   {id: '2', title: 'The Way of Kings', author: 'Brandon Sanderson'},
+  {id: "3", title: 'The Final Empire', author: 'Brandon Sanderson'},
+  {id: '4', title: 'The Way of Kings', author: 'Brandon Sanderson'},
 ];
 
 export default BOOKS_DATA;
